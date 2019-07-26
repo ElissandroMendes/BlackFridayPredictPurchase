e01642b34649fd9339dd9794b1f1169df67ad49c
elissandromendes <elissandromendes@gmail.com>
2019-07-26 13:11:51 -0300

    Trying better score...

--- blackfriday.py
+++ blackfriday.py
@@ -9,20 +9,26 @@ Created on Sat Jun 15 12:45:23 2019
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# Load data
 base = pd.read_csv("train.csv")
-data = base.drop(['User_ID', 'Product_ID', 'Product_Category_2', 'Product_Category_3'], axis=1)
+data = base.drop(['User_ID', 'Product_ID', 'Product_Category_2', 
+                  'Product_Category_3', 'Gender', 'City_Category',
+                  'Stay_In_Current_City_Years'], axis=1)
 
+# Separate features and target
 X = data.iloc[:, :-1].values
-y = data.iloc[:, 7].values
-y = y.reshape(-1, 1)
+y = data.iloc[:, 4].values
+# y = y.reshape(-1, 1)
 
+# Treat categorical features, in order, using label encoder
 from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
 X[:, 0] = encoder.fit_transform(X[:, 0])
 X[:, 1] = encoder.fit_transform(X[:, 1])
-X[:, 3] = encoder.fit_transform(X[:, 3])
-X[:, 4] = encoder.fit_transform(X[:, 4])
+# X[:, 3] = encoder.fit_transform(X[:, 3])
+# X[:, 4] = encoder.fit_transform(X[:, 4])
 
+# Split data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
@@ -30,4 +36,13 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 regressor.score(X_train, y_train)
-regressor.predict(X_test)
+
+forecasts = regressor.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+mae = mean_absolute_error(y_test, forecasts)
+
+
+
+
+
